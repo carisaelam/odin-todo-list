5f0a97ede013a7ae93f3e88eaa391d94b637183c
carisaelam <carisaelam@gmail.com>
2024-10-23 16:47:07 -0400

    Add project input now adds to dropdown

--- src/index.js
+++ src/index.js
@@ -10,35 +10,51 @@ const dueDateInput = document.getElementById('due-date');
 const projectInput = document.getElementById('project');
 const newProjectInput = document.getElementById('new-project');
 const projectDisplay = document.querySelector('.project__display');
+const newProjectForm = document.querySelector('.new__project__form');
 
 const inbox = new Project('Inbox');
-const workProject = new Project('Work');
-const personalProject = new Project('Personal');
+const tester = new Project('Tester');
+const projects = [];
 
-const projects = [inbox, workProject, personalProject];
+console.log('projects', projects);
 
-projectInput.addEventListener('change', (e) => {
-  if (e.target.value === 'new') {
-    newProjectInput.style.display = 'block';
-  } else {
-    newProjectInput.style.display = 'none';
-    newProjectInput.value = '';
-  }
+function addProjectsToDropdown(project) {
+  const projectOption = document.createElement('option');
+  projectOption.innerHTML = `${project.title}`;
+  projectInput.appendChild(projectOption);
+  projects.push(project);
+}
+
+addProjectsToDropdown(inbox);
+
+newProjectForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(projects);
+  const newProject = new Project(newProjectInput.value);
+  addProjectsToDropdown(newProject);
 });
 
 addTaskForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
+  // Sets selectedProject as the element in the projects array that matches the project.title
   let selectedProject = projects.find(
     (project) =>
       project.title.toLowerCase() === projectInput.value.toLowerCase()
   );
 
-  if (projectInput.value === 'new' && newProjectInput.value) {
-    selectedProject = new Project(newProjectInput.value);
-    projects.push(selectedProject);
+  if (!selectedProject) {
+    selectedProject = inbox;
   }
 
+  console.log('selected project', selectedProject);
+
+  // Pushes new project into projects array
+  // if (projectInput.value === 'new' && newProjectInput.value) {
+  //   createProject(newProjectInput.value);
+  // }
+
+  // Creates a new Task with user input and selectedProject
   const task = new Task(
     titleInput.value,
     descriptionInput.value,
@@ -47,16 +63,16 @@ addTaskForm.addEventListener('submit', (e) => {
     undefined,
     selectedProject
   );
-  console.log(task);
+
   selectedProject.addTaskToProject(task);
   selectedProject.listTasks();
-  const taskListElement1 = tasksListed(inbox);
-  projectDisplay.textContent = '';
-  projectDisplay.appendChild(taskListElement1);
 
+  projectDisplay.textContent = '';
+
+  // Calls tasksListed for each project in the array
   projects.forEach((project) => {
-    const taskListElement1 = tasksListed(project);
-    projectDisplay.appendChild(taskListElement1);
+    const taskListElement = tasksListed(project);
+    projectDisplay.appendChild(taskListElement);
   });
 
   titleInput.value = '';
@@ -65,24 +81,7 @@ addTaskForm.addEventListener('submit', (e) => {
   projectInput.value = '';
 });
 
-const task1 = new Task('First task', 'first description', '2024-12-31');
-const task2 = new Task('Second task', 'second description', '2024-12-30');
-const task3 = new Task('Third task', 'third description', '2024-12-29');
-const task4 = new Task('Fourth task', 'fourth description', '2024-12-28');
-const task5 = new Task('Fifth task', 'fifth description', '2024-12-20', 1);
-const task6 = new Task('Sixth task', 'sixth description', '2024-12-18', 2);
-const task7 = new Task('Seventh task', 'seventh description', '2025-01-01');
-
-// inbox.addTaskToProject(task1);
-// inbox.addTaskToProject(task2);
-// inbox.addTaskToProject(task3);
-// inbox.addTaskToProject(task4);
-// inbox.addTaskToProject(task5);
-// inbox.addTaskToProject(task6);
-// inbox.addTaskToProject(task7);
-
-console.log('inbox: ', inbox);
-
+// Creates and appends html for projects
 function tasksListed(project) {
   const tasks = project.listTasks();
   const html = document.createElement('div');
@@ -107,3 +106,11 @@ function tasksListed(project) {
 
   return html;
 }
+
+const task1 = new Task('First task', 'first description', '2024-12-31');
+const task2 = new Task('Second task', 'second description', '2024-12-30');
+const task3 = new Task('Third task', 'third description', '2024-12-29');
+const task4 = new Task('Fourth task', 'fourth description', '2024-12-28');
+const task5 = new Task('Fifth task', 'fifth description', '2024-12-20', 1);
+const task6 = new Task('Sixth task', 'sixth description', '2024-12-18', 2);
+const task7 = new Task('Seventh task', 'seventh description', '2025-01-01');
